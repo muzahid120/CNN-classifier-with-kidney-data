9e31e59c4c02a51fb12651dc8e2acf27a0ba6dbc
muzahid120 <muzahidsk771@gmail.com>
2025-03-12 10:26:00 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def get_requirements(file_path:str) -> List[str]:
 
 
 setup(
-name='CNN project',
+name='CNN_project',
 version="0.0.1",
 author='Sk Mozahid',
 author_email='muzahidsk771@gmail.com',
